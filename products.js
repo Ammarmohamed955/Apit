d2028fda35f9379a3eb9370751d54fc240b28b8c
Ammarmohamed955 <johnymartin9660@gmail.com>
2024-09-12 03:57:59 +0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,65 @@
+const products = [
+  {
+    id: 1,
+    productName: "  Iphone",
+    description:
+      "Apple iPhone 15 Pro Max with FaceTime - 512GB, 8GB RAM",
+    price:  900,
+    rating: 5,
+    isLap: false,
+    imageLink: [
+      "https://res.cloudinary.com/dfgeiuhwv/image/upload/v1724298839/3-copy-6_reryvs.webp",
+      "https://res.cloudinary.com/dfgeiuhwv/image/upload/v1724298838/Apple-IPhone-15-Pro-Max-With-FaceTime-512GB-8GB-RAM_Apple_5278_2_tflpej.jpg",
+     "https://res.cloudinary.com/dfgeiuhwv/image/upload/v1724298837/%D9%A6-copy-1_zwsfbm.webp",
+      "https://res.cloudinary.com/dfgeiuhwv/image/upload/v1724298837/2-copy-6_aywbw0.webp",
+    ],
+  },
+  {
+    id: 2,
+    productName: "Smart Watch",
+    description:
+      "The Hainoteko Germany Smart Watch SQ-10 Ultra 2 offers advanced features, including fitness tracking, notifications, and customizable watch faces.",
+      
+    price: 200,
+    rating: 4,
+    isLap: false,
+    imageLink: [
+      "https://res.cloudinary.com/dfgeiuhwv/image/upload/v1724296372/banner-image_cywt1c.png",
+      "https://res.cloudinary.com/dfgeiuhwv/image/upload/v1724296372/banner-image_cywt1c.png",
+      "https://res.cloudinary.com/dfgeiuhwv/image/upload/v1724296340/insta-item2_socryl.jpg",
+      "https://res.cloudinary.com/dfgeiuhwv/image/upload/v1724296260/product-item10_wacoyb.jpg",
+    ],
+  },
+  {
+    id: 3,
+    productName: "PlayStation",
+    description:
+      "the PlayStation 5 is Ahome vidio game console debvloper by sony, it was announced as the succcessor to the playStation 4",
+    price: 500,
+    rating: 5,
+    isLap: false,
+    imageLink: [
+      "https://res.cloudinary.com/dfgeiuhwv/image/upload/v1724299603/PS5-Hero-1_aqamq5.webp",
+      "https://res.cloudinary.com/dfgeiuhwv/image/upload/v1724299603/PS5-Hero-3_g6bhbs.webp",
+      "https://res.cloudinary.com/dfgeiuhwv/image/upload/v1724299603/ps5-slim-model-hero-new_ncyn75.webp",
+      "https://res.cloudinary.com/dfgeiuhwv/image/upload/v1724299603/PS5-SLIM-Hero-Box-US-V2_u3uyis.webp",
+    ],
+  },
+  {
+    id: 4,
+    productName: "Labtop",
+    description:
+      "Product description HP 5C1B9EA 15.6´´ i3-1155G7/8GB/512GB SSD laptop",
+    price: 1200,
+    rating: 4,
+    isLap: true,
+    imageLink: [
+      "https://res.cloudinary.com/dfgeiuhwv/image/upload/v1724300807/hp-5c1b9ea-15.6-i3-1155g7-8gb-512gb-ssd-laptop_3_r6nbcg.webp",
+      "https://res.cloudinary.com/dfgeiuhwv/image/upload/v1724300862/hp-5c1b9ea-15.6-i3-1155g7-8gb-512gb-ssd-laptop_ntv193.webp",
+      "https://res.cloudinary.com/dfgeiuhwv/image/upload/v1724300861/hp-5c1b9ea-15.6-i3-1155g7-8gb-512gb-ssd-laptop_2_sgrvna.webp",
+      "https://res.cloudinary.com/dfgeiuhwv/image/upload/v1724300807/hp-5c1b9ea-15.6-i3-1155g7-8gb-512gb-ssd-laptop_3_r6nbcg.webp",
+     
+    ],
+  },
+];
+module.exports = products;
