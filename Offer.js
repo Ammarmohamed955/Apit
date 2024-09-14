1572abd85fce2f3d686e2f301a91822b4fb9a71b
Ammarmohamed955 <johnymartin9660@gmail.com>
2024-09-14 05:01:39 +0300

    Add files via upload

---
+++ Offer.js
@@ -0,0 +1,43 @@
+const Offer =[
+  {
+  id: 1,
+  productName: "Iphone",
+  price:  900,
+    rating: 5,
+    offer: 320,
+    discount: "30%",
+ imageLink: [
+   "https://res.cloudinary.com/dqzplmmrj/image/upload/v1726276738/35_2_xr6nr1.png",
+   "https://res.cloudinary.com/dqzplmmrj/image/upload/v1726276807/35_1_ioeiso.png",
+   "https://res.cloudinary.com/dqzplmmrj/image/upload/v1726276807/35_mght7g.png"
+   ],
+   
+},
+{
+  productName: "Smart Watch",
+  price:  200,
+    rating: 4,
+    offer: 180,
+    discount: "20%",
+  imageLink: [
+    "https://res.cloudinary.com/dqzplmmrj/image/upload/v1726278061/20_oxqamu.png"
+    
+    ],
+    
+  
+  
+  
+},
+{
+  productName: "PlayStation",
+  price:  500,
+    rating: 5,
+    offer: 475,
+    discount: "5%",
+    imageLink: [
+      "https://res.cloudinary.com/dqzplmmrj/image/upload/v1726278545/OFF_sbxmah.png",
+      "https://res.cloudinary.com/dqzplmmrj/image/upload/v1726279037/zh467_yoxlgq.png"
+      ]
+}
+  ]
+  module.exports = Offer;
